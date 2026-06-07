c3a87bff08f26d0e9a9215d5c46b81a9ef129d6d
dhanshew72 <d.hanshew@aol.com>
2026-06-06 20:33:31 -0700

    Added module for running locally via command line

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"ssse-exercise-sieve/pkg/sieve"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("usage: sieve <n>")
+	}
+	// Parses string for base 10 for 64 bit size
+	num, err := strconv.ParseInt(os.Args[1], 10, 64)
+	if err != nil {
+		log.Fatalf("invalid input: %v", err)
+	}
+	result, err := sieve.NewSieve().NthPrime(num)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Println("Nth Prime Result: ", result)
+}
